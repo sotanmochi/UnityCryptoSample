478ff3f78782de1b98b84b26b5e170b24edf6c13
sotanmochi <stmczk@gmail.com>
2021-04-21 23:52:24 +0900

    Update samples

--- Assets/CryptoSample/Scripts/DecryptionSample.cs
+++ Assets/CryptoSample/Scripts/DecryptionSample.cs
@@ -1,21 +1,86 @@
-﻿using UnityEngine;
-using System.IO;
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class DecryptionSample : MonoBehaviour
+namespace Crypto.Sample
 {
-	private string password = "test";
-
-	void Start()
+	public class DecryptionSample : MonoBehaviour
 	{
-		string filename = "encrypted.txt";
-		string path = Application.streamingAssetsPath + "/Text/" + filename;
-		byte[] encryptedData = File.ReadAllBytes(path);
+		[SerializeField] Button _StartButton;
 
-		byte[] decryptedData = RijndaelDecryptor.Decrypt(encryptedData, password);
-		if (decryptedData != null)
+		[SerializeField] FolderType _FolderType;
+		[SerializeField] string _SubFolder;
+		[SerializeField] string _FileName;
+		[SerializeField] string _Password = "test";
+
+		private string _FolderPath = Application.streamingAssetsPath;
+
+		void Start()
 		{
-			string decryptedText = System.Text.Encoding.UTF8.GetString(decryptedData);
-			Debug.Log("DecryptedText: " + decryptedText);
+			_StartButton.onClick.AddListener(() => 
+			{
+				string fileFullPath = GetFilePath();
+				if (fileFullPath != null)
+				{
+					DecryptTest(fileFullPath);
+				}
+			});
+		}
+
+		private async void DecryptTest(string fileFullPath)
+		{
+			Debug.Log("***** Start *****");
+
+			Debug.Log("***** ReadAsync *****");
+			byte[] encryptedData;
+			using (var sourceStream = new FileStream(fileFullPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+			{
+				encryptedData = new byte[sourceStream.Length];
+				await sourceStream.ReadAsync(encryptedData, 0, encryptedData.Length);
+			}
+
+			Debug.Log("***** Decrypt *****");
+			byte[] decryptedData = RijndaelDecryptor.Decrypt(encryptedData, _Password);
+
+			if (decryptedData != null)
+			{
+				Debug.Log("***** WriteAsync *****");
+
+				Directory.CreateDirectory($"{_FolderPath}/Decrypted/{_SubFolder}");
+				string savePath = $"{_FolderPath}/Decrypted/{_SubFolder}/{_FileName}";
+
+				using (var sourceStream = new FileStream(savePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 4096, true))
+				{
+					await sourceStream.WriteAsync(decryptedData, 0, decryptedData.Length);
+				}
+
+				Debug.Log("***** Decrypted data has been saved *****");
+			}
+		}
+
+		private string GetFilePath()
+		{
+			switch (_FolderType)
+			{
+				case FolderType.StreamingAssets:
+					_FolderPath = Application.streamingAssetsPath;
+					break;
+				case FolderType.Desktop:
+					_FolderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+					break;
+				default:
+					_FolderPath = Application.streamingAssetsPath;
+					break;
+			}
+
+			string filePath =  $"{_FolderPath}/{_SubFolder}/{_FileName}";
+			if (!File.Exists(filePath))
+			{
+				Debug.LogError("File not found");
+				return null;
+			}
+
+			return filePath;
 		}
 	}
 }
--- Assets/CryptoSample/Scripts/EncryptionSample.cs
+++ Assets/CryptoSample/Scripts/EncryptionSample.cs
@@ -1,22 +1,92 @@
-﻿using UnityEngine;
-using System.IO;
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class EncryptionSample : MonoBehaviour
+namespace Crypto.Sample
 {
-	private string password = "test";
-
-	void Start()
+	public enum FolderType
 	{
-		string filename = "sample.txt";
-		string path = Application.streamingAssetsPath + "/Text/" + filename;
-		byte[] srcData = File.ReadAllBytes(path);
+		StreamingAssets,
+		Desktop,
+	};
 
-		byte[] encryptedData = RijndaelEncryptor.Encrypt(srcData, password);
-		if (encryptedData != null)
+	public class EncryptionSample : MonoBehaviour
+	{
+		[SerializeField] Button _StartButton;
+
+		[SerializeField] FolderType _FolderType;
+		[SerializeField] string _SubFolder;
+		[SerializeField] string _FileName;
+		[SerializeField] string _Password = "test";
+
+		private string _FolderPath = Application.streamingAssetsPath;
+
+		void Start()
 		{
-			string savepath = Application.streamingAssetsPath + "/Text/encrypted.txt";
-			File.WriteAllBytes(savepath, encryptedData);
-			Debug.Log("Encrypted data is saved.");
+			_StartButton.onClick.AddListener(() => 
+			{
+				string fileFullPath = GetFilePath();
+				if (fileFullPath != null)
+				{
+					EncryptTest(fileFullPath);
+				}
+			});
+		}
+
+		private async void EncryptTest(string fileFullPath)
+		{
+			Debug.Log("***** Start *****");
+
+			Debug.Log("***** ReadAsync *****");
+			byte[] data;
+			using (var sourceStream = new FileStream(fileFullPath, FileMode.Open, FileAccess.Read, FileShare.Read, 8192, true))
+			{
+				data = new byte[sourceStream.Length];
+				await sourceStream.ReadAsync(data, 0, data.Length);
+			}
+
+			Debug.Log("***** Encrypt *****");
+			byte[] encryptedData = RijndaelEncryptor.Encrypt(data, _Password);
+
+			if (encryptedData != null)
+			{
+				Debug.Log("***** WriteAsync *****");
+
+				Directory.CreateDirectory($"{_FolderPath}/Encrypted/{_SubFolder}");
+				string savePath = $"{_FolderPath}/Encrypted/{_SubFolder}/{_FileName}";
+
+				using (var sourceStream = new FileStream(savePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 8192, true))
+				{
+					await sourceStream.WriteAsync(encryptedData, 0, encryptedData.Length);
+				}
+
+				Debug.Log("***** Encrypted data has been saved *****");
+			}
+		}
+
+		private string GetFilePath()
+		{
+			switch (_FolderType)
+			{
+				case FolderType.StreamingAssets:
+					_FolderPath = Application.streamingAssetsPath;
+					break;
+				case FolderType.Desktop:
+					_FolderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+					break;
+				default:
+					_FolderPath = Application.streamingAssetsPath;
+					break;
+			}
+
+			string filePath =  $"{_FolderPath}/{_SubFolder}/{_FileName}";
+			if (!File.Exists(filePath))
+			{
+				Debug.LogError("File not found");
+				return null;
+			}
+
+			return filePath;
 		}
 	}
 }
